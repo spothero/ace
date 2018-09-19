033621d23cf306a1f0fdbfcdf637528b66ec2bd5
Matt Przybylski <mprzybylski@gmail.com>
2018-09-19 13:56:39 -0700

    Babel 7 and React 16.4.2 (#7)
    * upgrade: Dependency upgrades in preparation for Babel 7, React upgraded to 16.4.2
    
    * breaking: Upgrade to Babel 7. Update `babel-polyfill` import statement path to `@babel/polyfill`.

--- config/babel.js
+++ config/babel.js
@@ -4,17 +4,40 @@ const browserslist = require('./browserslist');
 const defaults = getRCValues({
     babel: {
         presets: [
-            ['env', {
+            ['@babel/preset-env', {
                 targets: {
                     browsers: browserslist,
                     node: 'current'
                 },
-                useBuiltIns: true
+                useBuiltIns: 'entry'
             }],
-            'react',
-            'stage-1'
+            '@babel/preset-react'
         ],
         plugins: [
+            '@babel/plugin-transform-runtime',
+
+            // Stage 1
+            '@babel/plugin-proposal-export-default-from',
+            '@babel/plugin-proposal-logical-assignment-operators',
+            '@babel/plugin-proposal-optional-chaining',
+            ['@babel/plugin-proposal-pipeline-operator', {proposal: 'minimal'}],
+            '@babel/plugin-proposal-nullish-coalescing-operator',
+            '@babel/plugin-proposal-do-expressions',
+
+            // Stage 2
+            ['@babel/plugin-proposal-decorators', {legacy: true}],
+            '@babel/plugin-proposal-function-sent',
+            '@babel/plugin-proposal-export-namespace-from',
+            '@babel/plugin-proposal-numeric-separator',
+            '@babel/plugin-proposal-throw-expressions',
+
+            // Stage 3
+            '@babel/plugin-syntax-dynamic-import',
+            '@babel/plugin-syntax-import-meta',
+            '@babel/plugin-proposal-class-properties',
+            '@babel/plugin-proposal-json-strings',
+
+            // React
             'react-hot-loader/babel'
         ]
     }
