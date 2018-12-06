b51ab3e31f920db868f79765aad80193346582f7
Matt Przybylski <mprzybylski@gmail.com>
2018-12-06 14:07:38 -0700

    feat: Add CSS filename key/value pairs to the manifest during development builds

---
+++ config/gulp/tasks/css-manifest.js
@@ -0,0 +1,27 @@
+const gulp = require('gulp');
+const rev = require('gulp-rev');
+const through = require('through2');
+const modify = require('modify-filename');
+const projectPath = require('../lib/project-path');
+
+const cssManifestTask = () => {
+    const dest = `${projectPath(global.SETTINGS_CONFIG.root.path)}/${global.SETTINGS_CONFIG.dist.path}`;
+
+    return gulp.src(`${dest}/${global.SETTINGS_CONFIG.dist.css.path}/*.css`, {base: dest})
+        .pipe(rev())
+        .pipe(through.obj((file, enc, cb) => {
+            // see: https://github.com/sindresorhus/gulp-rev/issues/186#issuecomment-229279896
+            // don't alter the css file paths for non-production builds
+            file.path = modify(file.revOrigPath, (name, ext) => {
+                return `${name}${ext}`;
+            });
+
+            cb(null, file);
+        }))
+        .pipe(rev.manifest(`${dest}/${global.SETTINGS_CONFIG.dist.manifestFilename}`, {
+            base: dest
+        }))
+        .pipe(gulp.dest(dest));
+};
+
+gulp.task('cssManifest', cssManifestTask);
