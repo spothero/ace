27c31024187f12d1bf9c7edc214fffa250af9384
Matt Przybylski <mprzybylski@gmail.com>
2018-12-14 08:18:45 -0700

    refactor: Add wait-on to ssr dev task to avoid errors on first run

--- config/gulp/tasks/dev-server.js
+++ config/gulp/tasks/dev-server.js
@@ -13,7 +13,7 @@ const devServerSSRTask = () => {
     const serverPath = `${global.SETTINGS_CONFIG.dist.path}/${global.SETTINGS_CONFIG.webpack.server.output}`;
 
     return shell.task([
-        `webpack --watch --hide-modules --config ${configPath} & nodemon ${serverPath}`
+        `webpack --watch --hide-modules --config ${configPath} & wait-on ${serverPath} && nodemon ${serverPath}`
     ], {
         cwd: process.env.INIT_CWD
     });
