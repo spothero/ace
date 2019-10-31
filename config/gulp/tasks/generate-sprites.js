76dd0f85f377cec919f21a092b87c9e1e0c07f06
Matt Przybylski <mprzybylski@gmail.com>
2019-10-31 10:01:36 -0700

    fix: Change generateSprites padding to be 4 instead of 5 for more rounded generated numbers

--- config/gulp/tasks/generate-sprites.js
+++ config/gulp/tasks/generate-sprites.js
@@ -6,11 +6,13 @@ const colors = require('ansi-colors');
 const stream = require('merge-stream')();
 const projectPath = require('../lib/project-path');
 
-const padding = 5;
+const padding = 4;
 const cssTemplate = './spritesmith.template.mustache';
 
 const generateSpritesTask = () => {
-    const src = `${projectPath(global.SETTINGS_CONFIG.root.path)}/${global.SETTINGS_CONFIG.src.path}`;
+    const src = `${projectPath(global.SETTINGS_CONFIG.root.path)}/${
+        global.SETTINGS_CONFIG.src.path
+    }`;
     const spriteSrc = global.SETTINGS_CONFIG.src.sprites.srcPath;
     const spriteDest = `${src}/${global.SETTINGS_CONFIG.src.img.path}/${global.SETTINGS_CONFIG.src.sprites.outputPath}`;
     const sassDest = `${src}/${global.SETTINGS_CONFIG.src.sass.path}/${global.SETTINGS_CONFIG.src.sprites.sassMapOutputPath}`;
@@ -20,31 +22,33 @@ const generateSpritesTask = () => {
     spriteNames.forEach(name => {
         spriteIds[name] = [];
 
-        const retina = gulp.src(`${src}/${spriteSrc}/${name}-2x/*.png`)
-            .pipe(spritesmith({
+        const retina = gulp.src(`${src}/${spriteSrc}/${name}-2x/*.png`).pipe(
+            spritesmith({
                 imgName: `sprite-${name}@2x.png`,
                 cssName: `_sprites-${name}-map-2x.scss`,
                 cssTemplate,
                 cssOpts: {
-                    sprite_type: `${name}-2x` // eslint-disable-line camelcase
+                    sprite_type: `${name}-2x`, // eslint-disable-line camelcase
                 },
                 cssVarMap: sprite => {
                     spriteIds[name].push(sprite.name);
                 },
-                padding
-            }));
+                padding,
+            })
+        );
         const retinaImageStream = retina.img.pipe(gulp.dest(spriteDest));
         const retinaCSSStream = retina.css.pipe(gulp.dest(sassDest));
-        const regular = gulp.src(`${src}/${spriteSrc}/${name}-1x/*.png`)
-            .pipe(spritesmith({
+        const regular = gulp.src(`${src}/${spriteSrc}/${name}-1x/*.png`).pipe(
+            spritesmith({
                 imgName: `sprite-${name}.png`,
                 cssName: `_sprites-${name}-map.scss`,
                 cssTemplate,
                 cssOpts: {
-                    sprite_type: name // eslint-disable-line camelcase
+                    sprite_type: name, // eslint-disable-line camelcase
                 },
-                padding
-            }));
+                padding,
+            })
+        );
         const regularImageStream = regular.img.pipe(gulp.dest(spriteDest));
         const regularCSSStream = regular.css.pipe(gulp.dest(sassDest));
 
@@ -55,8 +59,20 @@ const generateSpritesTask = () => {
     });
 
     stream.on('end', () => {
-        log(colors.yellow(`Combined sprite @1x and @2x images have been generated in ${colors.green(spriteDest)}.`));
-        log(colors.yellow(`Sass map information files have been generated in ${colors.green(sassDest)}.`));
+        log(
+            colors.yellow(
+                `Combined sprite @1x and @2x images have been generated in ${colors.green(
+                    spriteDest
+                )}.`
+            )
+        );
+        log(
+            colors.yellow(
+                `Sass map information files have been generated in ${colors.green(
+                    sassDest
+                )}.`
+            )
+        );
 
         spriteNames.forEach(name => {
             const sassSpritesOutputPath = `${src}/${global.SETTINGS_CONFIG.src.sass.path}/${global.SETTINGS_CONFIG.src.sprites.sassSpritesOutputPath}`;
@@ -75,13 +91,23 @@ const generateSpritesTask = () => {
 
             sassStream.write(spriteStr, 'utf8');
             sassStream.on('finish', () => {
-                log(colors.yellow(`Created ${colors.green(sassFileName)}. This file should be imported into your "_sprites.scss" mixin.`));
+                log(
+                    colors.yellow(
+                        `Created ${colors.green(
+                            sassFileName
+                        )}. This file should be imported into your "_sprites.scss" mixin.`
+                    )
+                );
                 log(colors.yellow(`@import "[path-to-file]/sprites-${name}";`));
             });
             sassStream.end();
         });
 
-        log(colors.red('See https://spothero.com/uniform/ace/docs/tasks-sprites/ for more details.'));
+        log(
+            colors.red(
+                'See https://spothero.com/uniform/ace/docs/tasks-sprites/ for more details.'
+            )
+        );
     });
 
     return stream;
