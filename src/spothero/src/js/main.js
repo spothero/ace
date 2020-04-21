c659469e450e20e59cfb215c5f7cc407eac74e44
Matt <mprzybylski@gmail.com>
2020-04-21 12:25:03 -0700

    fix: Scaffold files no longer needing HTML plugin and import Sass to main JS file

--- src/spothero/src/js/main.js
+++ src/spothero/src/js/main.js
@@ -5,6 +5,7 @@ import 'transitionEnd';
 import React from 'react';
 import {render} from 'react-dom';
 // import {hydrate} from 'react-dom'; // Use instead for SSR projects
+import '../sass/main.scss';
 import App from './App';
 
 render(<App />, document.querySelector('.root'));
