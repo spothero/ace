ac4f906864c67f6583f00bae56143fc9cd84d4f7
Boiar Qin <boiar@spothero.com>
2020-07-30 15:53:51 -0500

    fix: Only pull in known variables from process.env

--- config/webpack/utils.js
+++ config/webpack/utils.js
@@ -1,13 +1,35 @@
 const forEach = require('lodash/forEach');
 const settingsConfig = require('../gulp/lib/get-settings-config');
 
+const knownEnvVars = [
+    'ACE_ASSET_PATH',
+    'ACE_CONFIG_PATH',
+    'ACE_DEPLOY_TYPE',
+    'ACE_ENVIRONMENT',
+    'ACE_NPM_EVENT',
+    'BABEL_ENV',
+    'INIT_CWD',
+    'NODE_ENV',
+    'SANDBOX_NAME',
+];
+
 module.exports = {
     getEnvVars: environment => {
         const envVars = {};
 
-        forEach(process.env, (value, key) => {
-            envVars[key] = JSON.stringify(value);
-        });
+        // forEach(process.env, (value, key) => {
+        //     if (knownEnvVars.indexOf(key) >= 0) {
+        //         envVars[key] = JSON.stringify(value);
+        //     }
+        // });
+
+        forEach(knownEnvVars, (key) => {
+            const value = process.env[key];
+            
+            if (value) {
+                envVars[key] = JSON.stringify(value);
+            }
+        })
 
         return {
             ...envVars,
